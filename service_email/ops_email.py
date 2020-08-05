55ac932614440cac32e3618b0e325a65bf37ac3d
szhang <szhang@indocresearch.org>
2020-08-05 12:11:23 -0400

    add more logging information

--- service_email/ops_email.py
+++ service_email/ops_email.py
@@ -28,27 +28,31 @@ class Ops_email(Resource):
     @api.expect(query_payload)
     @api.response(200, query_sample_return)
     def post(self):
-        current_app.logger.info('start here')
-        try:
-            post_data = request.get_json()
-            sender = post_data.get('sender', None)
-            receiver = post_data.get('receiver', None)
-            message = post_data.get('message', None)  
-            current_app.logger.info(f'payload: {post_data}')
-            current_app.logger.info(f'receiver: {receiver}')
-            current_app.logger.info(f'message: {message}')
-            if not sender or not receiver or not message:
-                current_app.logger.exception('missing sender or receiver or message')
-                return {'result': 'missing sender or receiver or message'}, 400
+        current_app.logger.info('received request')
+        post_data = request.get_json()
+        sender = post_data.get('sender', None)
+        receiver = post_data.get('receiver', None)
+        message = post_data.get('message', None)  
+        current_app.logger.info(f'payload: {post_data}')
+        current_app.logger.info(f'receiver: {receiver}')
+        current_app.logger.info(f'message: {message}')
+        if not sender or not receiver or not message:
+            current_app.logger.exception('missing sender or receiver or message')
+            return {'result': 'missing sender or receiver or message'}, 400
+        try:    
             client = smtplib.SMTP('localhost')
-            fromaddr = sender
-            toaddrs = receiver
-            msg = message
+        except smtplib.socket.gaierror as e:
+            current_app.logger.exception(f'Error connecting with Mail host, {e}')
+            return {'result': str(e)}, 500
+        fromaddr = sender
+        toaddrs = receiver
+        msg = message
+        try:    
             client.sendmail(fromaddr, [toaddrs], msg)
-            client.quit()
         except Exception as e:
             current_app.logger.exception(f'Error when sending email to {receiver}, {e}')
             return {'result': str(e)}, 500
+        client.quit()
         current_app.logger.info(f'Email sent successfully to {receiver}')
         return {'result': "Email sent successfully. "}, 200
 
