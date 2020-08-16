65f4d21b6f3c34047530925781721dfe297f9d4b
zhiren zhan <zzhan@indocresearch.org>
2020-08-16 17:23:27 -0400

    Update ops_email.py

--- service_email/ops_email.py
+++ service_email/ops_email.py
@@ -41,7 +41,7 @@ class Ops_email(Resource):
             return {'result': 'missing sender or receiver or message'}, 400
         try:    
             client = smtplib.SMTP('10.3.9.240')
-            client.connect("10.3.9.240")
+            client.connect('10.3.9.240', 465)
             client.ehlo()
             client.starttls()
             client.ehlo()
