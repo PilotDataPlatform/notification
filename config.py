fbf1f419f104911831b9b4802136f1ca594580b1
szhang <szhang@indocresearch.org>
2020-08-21 19:05:00 -0400

    test on email service

--- config.py
+++ config.py
@@ -2,6 +2,7 @@
 class ConfigClass(object):
     # the packaged modules
     api_modules = ["service_email"]
-    postfix = 'external-postfix.utility'
+    postfix = 'mailout.easydns.com'
     smtp_user = 'oit.on.ca',
     smtp_pass = '1991alex'
+    smtp_port = 587
--- service_email/ops_email.py
+++ service_email/ops_email.py
@@ -40,11 +40,8 @@ class Ops_email(Resource):
             current_app.logger.exception('missing sender or receiver or message')
             return {'result': 'missing sender or receiver or message'}, 400
         try:
-            client = smtplib.SMTP(ConfigClass.postfix,25)
-            # client.ehlo()
-            # client.starttls()
-            # # client.connect()
-            # client.login(ConfigClass.smtp_user, ConfigClass.smtp_pass)
+            client = smtplib.SMTP(ConfigClass.postfix,ConfigClass.smtp_port)
+            client.login(ConfigClass.smtp_user, ConfigClass.smtp_pass)
             current_app.logger.info('email server connection established')
         except smtplib.socket.gaierror as e:
             current_app.logger.exception(f'Error connecting with Mail host, {e}')
