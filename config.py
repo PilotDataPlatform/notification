f5e815da4cafc1b43b45843cbad167bcfb2a9a65
szhang <szhang@indocresearch.org>
2020-08-21 19:08:31 -0400

    bug fix for configration

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ class ConfigClass(object):
     # the packaged modules
     api_modules = ["service_email"]
     postfix = 'mailout.easydns.com'
-    smtp_user = 'oit.on.ca',
+    smtp_user = 'oit.on.ca'
     smtp_pass = '1991alex'
     smtp_port = 587
