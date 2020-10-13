eb806af3afb7b4105d9cc1cc4df4e21107358013
jzhang <jzhang@indocresearh.org>
2020-10-13 15:12:12 -0400

    add log

--- tests/test_ops_email.py
+++ tests/test_ops_email.py
@@ -6,9 +6,15 @@ from requests import HTTPError
 import smtplib
 from smtplib import SMTP
 from app import create_app
+from tests.logger import Logger
 
 
 class TestWriteEmails(unittest.TestCase):
+    log_name = 'test_ops_email.log'
+    log = Logger(name=log_name)
+    log.warning("Removing old records")
+    log.debug("Test is ready to begin")
+    post_api = "/v1/email"
 
     def setUp(self):
         app = create_app()
@@ -23,9 +29,17 @@ class TestWriteEmails(unittest.TestCase):
         payload = {"sender": "notification@vre",
                    "receiver": "jzhang@indocresearch.org",
                    "message": "test email"}
-        response = self.app.post("/v1/email", json=payload)
-        self.assertEqual(response.status_code, 400)
-        self.assertTrue(path.exists('./logs'))
+        self.log.info('\n')
+        self.log.info('test post with correct payload'.center(80, '-'))
+        self.log.info(f"POST API: {self.post_api}")
+        self.log.info(f"POST PAYLOAD: {payload}")
+        response = self.app.post(self.post_api, json=payload)
+        try:
+            self.log.info(f"COMPARING: {response.status_code} VS {200}")
+            self.assertEqual(response.status_code, 200)
+        except Exception as e:
+            self.log.error(e)
+            raise e
 
     def test_post_no_sender(self):
         payload = {
@@ -33,9 +47,22 @@ class TestWriteEmails(unittest.TestCase):
             "receiver": "jzhang@indocresearch.org",
             "message": "test email"
         }
-        response = self.app.post("/v1/email", json=payload)
-        self.assertEqual(response.status_code, 400)
-        assert b"missing sender or receiver or message" in response.data
+        self.log.info('\n')
+        self.log.info('test post without sender in payload'.center(80, '-'))
+        self.log.info(f"POST API: {self.post_api}")
+        self.log.info(f"POST PAYLOAD: {payload}")
+        response = self.app.post(self.post_api, json=payload)
+        self.log.info(f"POST RESPONSE: {response}")
+        try:
+            self.log.info(f"COMPARING: {response.status_code} VS {400}")
+            self.assertEqual(response.status_code, 400)
+            self.log.info(f"COMPARING: {b'missing sender or receiver or message'}")
+            self.log.info("IN")
+            self.log.info(f"{response.data}")
+            assert b"missing sender or receiver or message" in response.data
+        except Exception as e:
+            self.log.error(e)
+            raise e
 
     def test_post_no_receiver(self):
         payload = {
@@ -43,11 +70,22 @@ class TestWriteEmails(unittest.TestCase):
             "receiver": None,
             "message": "test email"
         }
-        response = self.app.post("/v1/email", json=payload)
-        self.assertEqual(response.status_code, 400)
-        assert b"missing sender or receiver or message" in response.data
-        import smtplib
-        self.assertRaises(smtplib.socket.gaierror)
+        self.log.info('\n')
+        self.log.info('test post without receiver in payload'.center(80, '-'))
+        self.log.info(f"POST API: {self.post_api}")
+        self.log.info(f"POST PAYLOAD: {payload}")
+        response = self.app.post(self.post_api, json=payload)
+        self.log.info(f"POST RESPONSE: {response}")
+        try:
+            self.log.info(f"COMPARING: {response.status_code} VS {400}")
+            self.assertEqual(response.status_code, 400)
+            self.log.info(f"COMPARING: {b'missing sender or receiver or message'}")
+            self.log.info("IN")
+            self.log.info(f"{response.data}")
+            assert b"missing sender or receiver or message" in response.data
+        except Exception as e:
+            self.log.error(e)
+            raise e
 
     def test_post_no_message(self):
         payload = {
@@ -55,9 +93,22 @@ class TestWriteEmails(unittest.TestCase):
             "receiver": "jzhang@indocresearch.org",
             "message": None
         }
-        response = self.app.post("/v1/email", json=payload)
-        self.assertEqual(response.status_code, 400)
-        assert b"missing sender or receiver or message" in response.data
+        self.log.info('\n')
+        self.log.info('test post without message in payload'.center(80, '-'))
+        self.log.info(f"POST API: {self.post_api}")
+        self.log.info(f"POST PAYLOAD: {payload}")
+        response = self.app.post(self.post_api, json=payload)
+        self.log.info(f"POST RESPONSE: {response}")
+        try:
+            self.log.info(f"COMPARING: {response.status_code} VS {400}")
+            self.assertEqual(response.status_code, 400)
+            self.log.info(f"COMPARING: {b'missing sender or receiver or message'}")
+            self.log.info("IN")
+            self.log.info(f"{response.data}")
+            assert b"missing sender or receiver or message" in response.data
+        except Exception as e:
+            self.log.error(e)
+            raise e
 
     def test_html_email(self):
         html_msg = '''<!DOCTYPE html> \
@@ -69,35 +120,80 @@ class TestWriteEmails(unittest.TestCase):
                    "receiver": "jzhang@indocresearch.org",
                    "message": html_msg,
                    "msg_type": "html"}
-        response = self.app.post("/v1/email", json=payload)
-        self.assertEqual(response.status_code, 200)
-        self.assertTrue(path.exists('./logs'))
+        self.log.info('\n')
+        self.log.info('test post with html message type in payload'.center(80, '-'))
+        self.log.info(f"POST API: {self.post_api}")
+        self.log.info(f"POST PAYLOAD: {payload}")
+        response = self.app.post(self.post_api, json=payload)
+        self.log.info(f"POST RESPONSE: {response}")
+        try:
+            self.log.info(f"COMPARING: {response.status_code} VS {200}")
+            self.assertEqual(response.status_code, 200)
+        except Exception as e:
+            self.log.error(e)
+            raise e
 
     def test_wrong_message(self):
         payload = {"sender": "notification@vre",
                    "receiver": "jzhang@indocresearch.org",
                    "message": "test message",
                    "msg_type": "csv"}
-        response = self.app.post("/v1/email", json=payload)
-        self.assertEqual(response.status_code, 400)
-        assert b"wrong email type" in response.data
+        self.log.info('\n')
+        self.log.info('test post with other(csv) message type in payload'.center(80, '-'))
+        self.log.info(f"POST API: {self.post_api}")
+        self.log.info(f"POST PAYLOAD: {payload}")
+        response = self.app.post(self.post_api, json=payload)
+        self.log.info(f"POST RESPONSE: {response}")
+        try:
+            self.log.info(f"COMPARING: {response.status_code} VS {400}")
+            self.assertEqual(response.status_code, 400)
+            self.log.info(f"COMPARING: {b'wrong email type'}")
+            self.log.info("IN")
+            self.log.info(f"{response.data}")
+            assert b"wrong email type" in response.data
+        except Exception as e:
+            self.log.error(e)
+            raise e
 
     def test_multiple_receiver_list(self):
         payload = {"sender": "notification@vre",
                    "receiver": ["jzhang@indocresearch.org", "jiayu@indocresearch.org"],
                    "message": "test email"}
-        response = self.app.post("/v1/email", json=payload)
-        self.assertEqual(response.status_code, 200)
-        self.assertTrue(path.exists('./logs'))
+        self.log.info('\n')
+        self.log.info('test post with a list of receivers in payload'.center(80, '-'))
+        self.log.info(f"POST API: {self.post_api}")
+        self.log.info(f"POST PAYLOAD: {payload}")
+        response = self.app.post(self.post_api, json=payload)
+        self.log.info(f"POST RESPONSE: {response}")
+        try:
+            self.log.info(f"COMPARING: {response.status_code} VS {200}")
+            self.assertEqual(response.status_code, 200)
+        except Exception as e:
+            self.log.error(e)
+            raise e
 
     def test_list_receiver(self):
         payload = {"sender": "notification@vre",
                    "receiver": ["jzhang@indocresearch.org"],
                    "message": "test email"}
-        response = self.app.post("/v1/email", json=payload)
-        self.assertEqual(response.status_code, 200)
+        self.log.info('\n')
+        self.log.info('test post with receiver in the list format in payload'.center(80, '-'))
+        self.log.info(f"POST API: {self.post_api}")
+        self.log.info(f"POST PAYLOAD: {payload}")
+        response = self.app.post(self.post_api, json=payload)
+        self.log.info(f"POST RESPONSE: {response}")
+        try:
+            self.log.info(f"COMPARING: {response.status_code} VS {200}")
+            self.assertEqual(response.status_code, 200)
+        except Exception as e:
+            self.log.error(e)
+            raise e
 
     def test_logs(self):
+        self.log.info('\n')
+        self.log.warning("the logs folder will be remmoved for this testing")
+        self.log.info('test check if logs directory created'.center(80, '-'))
+        self.log.info(f"EXISTS OF LOGS FOLDER: {path.exists('./logs')}")
         self.assertTrue(path.exists('./logs'))
 
     @patch.object(smtplib, 'SMTP', side_effect=smtplib.socket.gaierror)
@@ -105,19 +201,39 @@ class TestWriteEmails(unittest.TestCase):
         payload = {"sender": "notification@vre",
                    "receiver": ["jzhang@indocresearch.org"],
                    "message": "test email"}
-        response = self.app.post("/v1/email", json=payload)
-        self.assertEqual(response.status_code, 500)
-        self.assertIsNotNone(response.data)
+        self.log.info('\n')
+        self.log.info('test smtplib.socket.gaierror occurred during posting'.center(80, '-'))
+        self.log.info(f"POST API: {self.post_api}")
+        self.log.info(f"POST PAYLOAD: {payload}")
+        response = self.app.post(self.post_api, json=payload)
+        self.log.info(f"POST RESPONSE: {response}")
+        try:
+            self.log.error("MOCK ERROR: smtplib.socket.gaierror")
+            self.log.info(f"COMPARING: {response.status_code} VS {500}")
+            self.assertEqual(response.status_code, 500)
+            self.log.info(f"CHECKING IS NOT NONE: {response.data}")
+            self.assertIsNotNone(response.data)
+        except Exception as e:
+            self.log.error(e)
+            raise e
 
     @patch.object(SMTP, 'sendmail', side_effect=HTTPError)
     def test_error(self, mock_smtp_send_error):
         payload = {"sender": "notification@vre",
                    "receiver": ["jzhang@indocresearch.org"],
                    "message": "test email"}
-        response = self.app.post("/v1/email", json=payload)
-        self.assertEqual(response.status_code, 500)
-        self.assertIsNotNone(response.data)
-
-
-if __name__ == "__main__":
-    unittest.main()
+        self.log.info('\n')
+        self.log.info('test unexpected error(HTTPError) occurred during posting'.center(80, '-'))
+        self.log.info(f"POST API: {self.post_api}")
+        self.log.info(f"POST PAYLOAD: {payload}")
+        response = self.app.post(self.post_api, json=payload)
+        self.log.info(f"POST RESPONSE: {response}")
+        try:
+            self.log.error("MOCK ERROR: requests.HTTPError")
+            self.log.info(f"COMPARING: {response.status_code} VS {500}")
+            self.assertEqual(response.status_code, 500)
+            self.log.info(f"CHECKING IS NOT NONE: {response.data}")
+            self.assertIsNotNone(response.data)
+        except Exception as e:
+            self.log.error(e)
+            raise e
