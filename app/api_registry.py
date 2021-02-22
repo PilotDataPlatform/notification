4fc3cb0a50aa9c5e4dbd753c13b50ff43ed0c750
Greg McCoy <gmccoy@indocresearch.org>
2021-02-22 09:43:54 -0500

    Adding route tags for swagger

--- app/api_registry.py
+++ app/api_registry.py
@@ -3,5 +3,5 @@ from .routers.v1 import api_announcement
 from .routers.v1.api_email import api_email
 
 def api_registry(app: FastAPI):
-    app.include_router(api_announcement.router, prefix="/v1/announcements")
-    app.include_router(api_email.router, prefix="/v1/email")
+    app.include_router(api_announcement.router, prefix="/v1/announcements", tags=["announcement"])
+    app.include_router(api_email.router, prefix="/v1/email", tags=["email"])
