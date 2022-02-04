bfca06e8902445c86fc7d9723dc269d5516f2112
Erik Taylor <etaylor@indocresearch.org>
2022-02-04 12:17:23 -0500

    Add duration check to put too

--- app/routers/v1/api_notification/api_notification.py
+++ app/routers/v1/api_notification/api_notification.py
@@ -78,6 +78,10 @@ class APINotification:
             api_response.set_error_msg('Message too long')
             api_response.set_code(EAPIResponseCode.bad_request)
             return api_response.json_response()
+        if int(data.detail.duration) <= 0:
+            api_response.set_error_msg('Duration less than or equal to zero')
+            api_response.set_code(EAPIResponseCode.bad_request)
+            return api_response.json_response()
         try:
             notification = db.session.query(NotificationModel).filter_by(id=id).first()
             notification.type = data.type
