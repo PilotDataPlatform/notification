0ef69266810c39115ebbcf0c941e411c1a535779
Erik Taylor <etaylor@indocresearch.org>
2022-02-11 10:42:15 -0500

    Remove unittest from test_notification.py

--- tests/test_notification.py
+++ tests/test_notification.py
@@ -1,4 +1,3 @@
-import unittest
 from json import loads
 
 import pytest
@@ -10,14 +9,16 @@ from tests.logger import Logger
 notificationId = None
 
 
-class TestNotification(unittest.TestCase):
+class TestNotification:
     log = Logger(name='test_notification.log')
     app = TestClient(app)
 
-    def setUp(self):
+    @classmethod
+    def setup_class(cls):
         pass
 
-    def tearDown(self):
+    @classmethod
+    def teardown_class(cls):
         pass
 
     @pytest.mark.dependency(name='test_01')
@@ -32,7 +33,7 @@ class TestNotification(unittest.TestCase):
         self.log.info(f'Response payload: {response.text}')
         global notificationId
         notificationId = loads(response.text)['result']['id']
-        self.assertEqual(response.status_code, 200)
+        assert response.status_code == 200
 
     @pytest.mark.dependency(depends=['test_01'])
     def test_02_get_notification(self):
@@ -40,7 +41,7 @@ class TestNotification(unittest.TestCase):
         params = {'id': notificationId}
         response = self.app.get('/v1/notification/', params=params)
         self.log.info(f'Response payload: {response.text}')
-        self.assertEqual(response.status_code, 200)
+        assert response.status_code == 200
 
     @pytest.mark.dependency(depends=['test_01'])
     def test_03_put_notification(self):
@@ -53,14 +54,14 @@ class TestNotification(unittest.TestCase):
         }
         response = self.app.put('/v1/notification/', params=params, json=payload)
         self.log.info(f'Response payload: {response.text}')
-        self.assertEqual(response.status_code, 200)
+        assert response.status_code == 200
 
     @pytest.mark.dependency(depends=['test_01'])
     def test_04_get_notifications(self):
         self.log.info('Test case 4: Get many notifications')
         response = self.app.get('/v1/notifications/')
         self.log.info(f'Response payload: {response.text}')
-        self.assertEqual(response.status_code, 200)
+        assert response.status_code == 200
 
     @pytest.mark.dependency(depends=['test_01'])
     def test_05_delete_notification(self):
@@ -68,7 +69,7 @@ class TestNotification(unittest.TestCase):
         params = {'id': notificationId}
         response = self.app.delete('/v1/notification/', params=params)
         self.log.info(f'Response payload: {response.text}')
-        self.assertEqual(response.status_code, 200)
+        assert response.status_code == 200
 
     def test_06_unsubscribe(self):
         self.log.info('Test case 6: Unsubscribe from notification with id=1')
@@ -78,4 +79,4 @@ class TestNotification(unittest.TestCase):
         }
         response = self.app.post('/v1/unsubscribe/', json=payload)
         self.log.info(f'Response payload: {response.text}')
-        self.assertEqual(response.status_code, 200)
+        assert response.status_code == 200
