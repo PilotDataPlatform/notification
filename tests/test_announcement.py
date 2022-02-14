331905968d4c6ce205c7fecbf2064bd3b7d05e26
Erik Taylor <etaylor@indocresearch.org>
2022-02-14 13:56:42 -0500

    Increase test coverage

---
+++ tests/test_announcement.py
@@ -0,0 +1,84 @@
+from json import loads
+
+import pytest
+from fastapi.testclient import TestClient
+
+from app.main import app
+from tests.logger import Logger
+
+
+class TestAnnouncement:
+    log = Logger(name='test_announcement.log')
+    app = TestClient(app)
+
+    @classmethod
+    def setup_class(cls):
+        pass
+
+    @classmethod
+    def teardown_class(cls):
+        pass
+
+    def test_01_post_announcement(self):
+        self.log.info('Test case 1: Post announcement')
+        payload = {
+            'project_code': 'test_01',
+            'content': 'Content for test announcement',
+            'publisher': 'erik'
+        }
+        response = self.app.post('/v1/announcements/', json=payload)
+        self.log.info(f'Response payload: {response.text}')
+        assert response.status_code == 200
+    
+    def test_02_get_announcements(self):
+        self.log.info(f'Test case 2: Get announcements with projectCode=test_01')
+        params = {
+            'project_code': 'test_01',
+            'order': 'asc',
+            'version': '1'
+        }
+        response = self.app.get('/v1/announcements/', params=params)
+        self.log.info(f'Response payload: {response.text}')
+        assert response.status_code == 200
+    
+    def test_03_post_announcements_message_too_long(self):
+        self.log.info('Test case 3: Post announcement; message too long')
+        payload = {
+            'project_code': 'test_03',
+            'content': 'Content for test announcement with a long message. Rem omnis ea sit. Aliquam omnis tempora est aliquam illo laborum. Mollitia voluptatem deserunt dolorem sapiente ad fugit minima tenetur. Atque qui corporis rerum veritatis aut. Et consectetur aut corporis earum cumque inventore occaecati rerum.',
+            'publisher': 'erik'
+        }
+        response = self.app.post('/v1/announcements/', json=payload)
+        self.log.info(f'Response payload: {response.text}')
+        assert response.status_code == 400
+    
+    def test_04_get_announcements_no_end_date(self):
+        self.log.info(f'Test case 4: Get announcements with projectCode=test_01; end date not provided')
+        params = {
+            'project_code': 'test_01',
+            'start_date': '2022-01-01'
+        }
+        response = self.app.get('/v1/announcements/', params=params)
+        self.log.info(f'Response payload: {response.text}')
+        assert response.status_code == 400
+    
+    def test_05_get_announcements_desc(self):
+        self.log.info(f'Test case 5: Get announcements with projectCode=test_01; descending order')
+        params = {
+            'project_code': 'test_01',
+            'order': 'desc'
+        }
+        response = self.app.get('/v1/announcements/', params=params)
+        self.log.info(f'Response payload: {response.text}')
+        assert response.status_code == 200
+    
+    def test_06_get_announcements_with_dates(self):
+        self.log.info(f'Test case 6: Get announcements with projectCode=test_01; between dates')
+        params = {
+            'project_code': 'test_01',
+            'start_date': '2022-01-01',
+            'end_date': '2022-12-31'
+        }
+        response = self.app.get('/v1/announcements/', params=params)
+        self.log.info(f'Response payload: {response.text}')
+        assert response.status_code == 200
--- tests/test_notification.py
+++ tests/test_notification.py
@@ -34,7 +34,7 @@ class TestNotification:
         global notificationId
         notificationId = loads(response.text)['result']['id']
         assert response.status_code == 200
-
+    
     @pytest.mark.dependency(depends=['test_01'])
     def test_02_get_notification(self):
         self.log.info(f'Test case 2: Get notification with id={notificationId}')
@@ -71,6 +71,7 @@ class TestNotification:
         self.log.info(f'Response payload: {response.text}')
         assert response.status_code == 200
 
+    @pytest.mark.dependency(name='test_06')
     def test_06_unsubscribe(self):
         self.log.info('Test case 6: Unsubscribe from notification with id=1')
         payload = {
@@ -78,5 +79,78 @@ class TestNotification:
             'notification_id': 1,
         }
         response = self.app.post('/v1/unsubscribe/', json=payload)
+        self.log.info(f'Response payload: {response.text}')
+        assert response.status_code == 200
+
+    def test_07_get_notification_not_exist(self):
+        self.log.info(f'Test case 7: Get notification with id=99999')
+        params = {'id': '99999'}
+        response = self.app.get('/v1/notification/', params=params)
+        self.log.info(f'Response payload: {response.text}')
+        assert response.status_code == 400
+    
+    def test_08_post_notification_message_too_long(self):
+        self.log.info('Test case 8: Post notification; message too long')
+        payload = {
+            'type': 'test_08',
+            'message': 'Test message from post with a long message. Rem omnis ea sit. Aliquam omnis tempora est aliquam illo laborum. Mollitia voluptatem deserunt dolorem sapiente ad fugit minima tenetur. Atque qui corporis rerum veritatis aut. Et consectetur aut corporis earum cumque inventore occaecati rerum.',
+            'detail': {'maintenance_date': '2022-01-20T15:20:13.955Z', 'duration': 1, 'duration_unit': 'h'},
+        }
+        response = self.app.post('/v1/notification/', json=payload)
+        self.log.info(f'Response payload: {response.text}')
+        assert response.status_code == 400
+
+    def test_09_post_notification_duration_not_positive(self):
+        self.log.info('Test case 9: Post notification; duration not positive')
+        payload = {
+            'type': 'test_09',
+            'message': 'Test message from post with an invalid duration',
+            'detail': {'maintenance_date': '2022-01-20T15:20:13.955Z', 'duration': -1, 'duration_unit': 'h'},
+        }
+        response = self.app.post('/v1/notification/', json=payload)
+        self.log.info(f'Response payload: {response.text}')
+        assert response.status_code == 400
+
+    @pytest.mark.dependency(depends=['test_01'])
+    def test_10_put_notification_message_too_long(self):
+        self.log.info(f'Test case 10: Put notification with id={notificationId}; message too long')
+        params = {'id': notificationId}
+        payload = {
+            'type': 'test_10',
+            'message': 'Test message from put with a long message. Rem omnis ea sit. Aliquam omnis tempora est aliquam illo laborum. Mollitia voluptatem deserunt dolorem sapiente ad fugit minima tenetur. Atque qui corporis rerum veritatis aut. Et consectetur aut corporis earum cumque inventore occaecati rerum.',
+            'detail': {'maintenance_date': '2022-01-20T15:20:13.955Z', 'duration': 1, 'duration_unit': 'h'},
+        }
+        response = self.app.put('/v1/notification/', params=params, json=payload)
+        self.log.info(f'Response payload: {response.text}')
+        assert response.status_code == 400
+
+    @pytest.mark.dependency(depends=['test_01'])
+    def test_11_put_notification_duration_not_positive(self):
+        self.log.info(f'Test case 10: Put notification with id={notificationId}; duration not positive')
+        params = {'id': notificationId}
+        payload = {
+            'type': 'test_11',
+            'message': 'Test message from put',
+            'detail': {'maintenance_date': '2022-01-20T15:20:13.955Z', 'duration': -1, 'duration_unit': 'h'},
+        }
+        response = self.app.put('/v1/notification/', params=params, json=payload)
+        self.log.info(f'Response payload: {response.text}')
+        assert response.status_code == 400
+
+    def test_12_get_notifications_no_username(self):
+        self.log.info('Test case 12: Get many notifications; username not provided')
+        params = {'all': False}
+        response = self.app.get('/v1/notifications/', params=params)
+        self.log.info(f'Response payload: {response.text}')
+        assert response.status_code == 400
+
+    @pytest.mark.dependency(depends=['test_06'])
+    def test_13_get_notifications_with_username(self):
+        self.log.info('Test case 13: Get many notifications; with username')
+        params = {
+            'all': False,
+            'username': 'erik'
+        }
+        response = self.app.get('/v1/notifications/', params=params)
         self.log.info(f'Response payload: {response.text}')
         assert response.status_code == 200
