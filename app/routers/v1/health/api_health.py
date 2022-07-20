cd684ab8aa5c402739b2e1c823cdb1896c771234
Erik Taylor <erikdvlp@protonmail.com>
2022-07-20 12:08:21 -0400

    Add health check

---
+++ app/routers/v1/health/api_health.py
@@ -0,0 +1,26 @@
+from common import LoggerFactory
+from app.models.sql_notification import NotificationModel
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.dependencies.db import get_db_session
+from fastapi import Depends
+from sqlalchemy.sql import select
+from app.models.sql_announcement import AnnouncementModel
+
+_logger = LoggerFactory('api_health').get_logger()
+
+async def opsdb_check(db: AsyncSession = Depends(get_db_session)):
+    try:
+        query = select(AnnouncementModel)
+        (await db.execute(query)).scalars().first()
+    except Exception as e:
+        _logger.error(f'Could not connect to notifications.announcement table: {e}')
+        return False
+    
+    try:
+        query = select(NotificationModel)
+        (await db.execute(query)).scalars().first()
+    except Exception as e:
+        _logger.error(f'Could not connect to notifications.system_maintenance table: {e}')
+        return False
+
+    return True
